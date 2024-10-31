cc3a73f23d346e429d9bc2684d2401989a6f9508
alex <alexandrepilarbrou@gmail.com>
2024-10-31 16:39:03 +0000

    update code to 2024 alex's standards

--- main.py
+++ main.py
@@ -1,294 +1,211 @@
-from itertools import combinations, permutations
-import copy
-import json
+from ortools.sat.python import cp_model
+import networkx as nx
+from tqdm import tqdm
+
+WITH_PRINT: bool = True
+
+ASCII_ART = """
+         o           o    o            o__ __o                                         o                   o__ __o                                                                   o                             
+        <|>         <|>  <|>          /v     v\                                       <|>                 /v     v\                                                                 <|>                            
+        / \         / \  / \         />       <\                                      / >                />       <\                                                                < >                            
+      o/   \o       \o/  \o/       o/               \o__ __o     o__ __o/  \o_ __o    \o__ __o         o/                 o__  __o   \o__ __o     o__  __o   \o__ __o     o__ __o/   |        o__ __o    \o__ __o  
+     <|__ __|>       |    |       <|       _\__o__   |     |>   /v     |    |    v\    |     v\       <|       _\__o__   /v      |>   |     |>   /v      |>   |     |>   /v     |    o__/_   /v     v\    |     |> 
+     /       \      / \  / \       \\          |    / \   < >  />     / \  / \    <\  / \     <\       \\          |    />      //   / \   / \  />      //   / \   < >  />     / \   |      />       <\  / \   < > 
+   o/         \o    \o/  \o/         \         /    \o/        \      \o/  \o/     /  \o/     o/         \         /    \o    o/     \o/   \o/  \o    o/     \o/        \      \o/   |      \         /  \o/       
+  /v           v\    |    |           o       o      |          o      |    |     o    |     <|           o       o      v\  /v __o   |     |    v\  /v __o   |          o      |    o       o       o    |        
+ />             <\  / \  / \          <\__ __/>     / \         <\__  / \  / \ __/>   / \    / \          <\__ __/>       <\/> __/>  / \   / \    <\/> __/>  / \         <\__  / \   <\__    <\__ __/>   / \       
+                                                                           \o/                                                                                                                                     
+                                                                            |                                                                                                                                      
+                                                                           / \                                                                                                                                     
+
+"""
 
 
-def saveToJson(xs, numberOfVertices):
-    with open('arraysOfVertex'+str(numberOfVertices), 'w') as outfile:
-        json.dump({"content": xs}, outfile)
+def get_interval_of_potential_edges(num_vertices: int) -> tuple[int, int]:
+    assert num_vertices > 0
+    min_number_of_edges = num_vertices - 1
+    max_number_of_edges = int((num_vertices * (num_vertices - 1)) / 2)
+
+    return min_number_of_edges, max_number_of_edges
 
 
-def checkIfGraphAlreadyInList(graph, graphList, numberOfVertices):
-    for oldGraph in graphList:
-        if compareGraph(graph, oldGraph, numberOfVertices):
-            return True
-    return False
+def get_all_degree_combinations(num_vertices: int, degree_sum: int) -> list[list[int]]:
+    def helper(degree_sum, num_vertices, max_val) -> list[list] | list:
+        if num_vertices == 0 and degree_sum == 0:
+            return [[]]
+        if num_vertices == 0 or degree_sum == 0:
+            return []
+        combinations = []
+        for i in range(min(degree_sum, max_val), 0, -1):
+            for combo in helper(degree_sum - i, num_vertices - 1, i):
+                combinations.append([i] + combo)
+        return combinations
+
+    return helper(degree_sum, num_vertices, num_vertices - 1)
 
 
-def compareGraph(originalGraph, newGraph, numberOfVertices):
-    if len(originalGraph) != len(newGraph):
-        return False
+class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
+    """Print intermediate solutions."""
 
-    for perm in permutations(range(1, numberOfVertices + 1)):
-        # print("PERMUTATION: ", perm)
-        isTheSame = True
-        for edge in newGraph:
-            shiftedEdge = [perm[edge[0] - 1], perm[edge[1] - 1]]
-            # print("original: ", edge, " || shifted: ", shiftedEdge)
-            if shiftedEdge not in originalGraph:
-                if shiftedEdge[::-1] not in originalGraph:
-                    # print("not the same")
-                    isTheSame = False
-                    break
-        if isTheSame:
-            return True
+    def __init__(self, variables):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.__variables = variables
+        self.__solution_count = 0
+        self.__solutionsArray = []
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+        curSol = []
+        for v in self.__variables:
+            # print('%s=%i' % (v, self.Value(v)), end=' ')
+            if self.Value(v) == 1:
+                vv = v.Name().split("_")[1:]
+                assert len(vv) == 2
+                curSol.append([int(v) for v in vv])
+        self.__solutionsArray.append(curSol)
         # print()
-        # input("...")
-    return False
+
+    def solution_count(self):
+        return self.__solution_count
+
+    def solutionArray(self):
+        return self.__solutionsArray
 
 
-def allCombs(numberOfVertices):
-    ret = []
-    for i in range(1, numberOfVertices + 1):
-        ret.append(list(allCombsWithSize(numberOfVertices, size=i)))
-    return ret
+def filter_out_isomorphic(all_graphs: list[list[tuple[int, int]]]) -> tuple[list, int]:
+    isomorphic_graphs: list[nx.Graph] = []
 
+    # this kinda depends on luck, but it might be a good indicator
+    # of useless computation after all isomorphic are found
+    how_many_useless_candidates_after_all_are_found: int = 0
 
-def allCombsWithSize(numberOfVertices, size):
-    xx = combinations(range(1, numberOfVertices), size)
-    return xx
-
-
-def initLengthCounter(numberOfVertices):
-    returner = {}
-    maxNumberOfEdges = (numberOfVertices * (numberOfVertices - 1)) / 2
-    for i in range(numberOfVertices - 1, int(maxNumberOfEdges + 1)):
-        returner[i] = 0
-    return returner
-
-
-def checkLengthRules(lengthCounter, newGraphLength, numberOfVertices):
-    # return True if the newGraph is not allowed
-    if newGraphLength == numberOfVertices - 1:
-        return lengthCounter[newGraphLength] >= numberOfVertices - 2
-    elif newGraphLength == ((numberOfVertices * (numberOfVertices - 1)) / 2) - 2:
-        return lengthCounter[newGraphLength] >= 2
-    elif newGraphLength == ((numberOfVertices * (numberOfVertices - 1)) / 2) - 1:
-        return lengthCounter[newGraphLength] >= 1
-    elif newGraphLength == (numberOfVertices * (numberOfVertices - 1)) / 2:
-        return lengthCounter[newGraphLength] >= 1
-
-
-def getAllGraphsWithNumberOfVertices(numberOfVertices, allConnected=True):
-    if allConnected:
-        graphsByVerticeNumber = {2: [[[[1, 2]]]]}
-    else:
-        graphsByVerticeNumber = {2: [[[1, 2]], []]}
-    assert isListOfLevels(graphsByVerticeNumber[2])
-    thisNumberOfVertices = 3
-    while thisNumberOfVertices <= numberOfVertices:
-        # print()
-        lengthCounter = initLengthCounter(thisNumberOfVertices)
-        print("NUMBER OF VERTICES: ", thisNumberOfVertices, "\n")
-        newOnes = []
-        newCombs = allCombs(thisNumberOfVertices)
-        if not allConnected:
-            newCombs.append([])
-        # print(thisNumberOfVertices, " -- ", newCombs)
-        counter = 0
-        for graphLevel in graphsByVerticeNumber[thisNumberOfVertices - 1]:
-            # print("Graph Level : ", graphLevel, "// length: ", len(graphLevel[0]))
-            # print("Graph length: ", len(graphLevel[0]))
-            assert isLevel(graphLevel)
-            for newCombsLevel in newCombs:
-                # print(
-                #     " ----  NewCombsLevel : ",
-                #     newCombsLevel,
-                #     "length: ",
-                #     len(newCombsLevel),
-                # )
-                if newCombsLevel == []:
-                    nCl = 0
-                else:
-                    nCl = len(newCombsLevel[0])
-                sumLength = nCl + len(graphLevel[0])
-                # print("SUM LENGTH: ", sumLength)
-                if not (sumLength in lengthCounter.keys()) or checkLengthRules(
-                    lengthCounter, sumLength, thisNumberOfVertices
-                ):
-                    # print("early continue")
-                    continue
-                for graph in graphLevel:
-                    assert isGraph(graph)
-                    # if counter % 10 == 0:
-                    #     print(
-                    #         "V num ",
-                    #         thisNumberOfVertices,
-                    #         " : ",
-                    #         counter
-                    #         / len(graphsByVerticeNumber[thisNumberOfVertices - 1]),
-                    #     )
-                    # print("USING GRAPH: ", graph)
-                    counter += 1
-                    # print("LEVEL ", newCombsLevel)
-                    for nn in newCombsLevel:
-                        newGraph = copy.deepcopy(graph)
-                        for nnn in nn:
-                            newGraph.append([thisNumberOfVertices, nnn])
-                        # print("testing for: ", newGraph)
-                        if checkLengthRules(
-                            lengthCounter, len(newGraph), thisNumberOfVertices
-                        ):
-                            # print("NOT, BREAKED")
-                            # print(len(newCombsLevel) + len(graphLevel[0]))
-                            break
-
-                        if not checkIfGraphAlreadyInList(
-                            newGraph, newOnes, thisNumberOfVertices
-                        ):
-                            # print("good! added")
-                            newOnes.append(newGraph)
-                            # print("                      ", newGraph)
-                            lengthCounter[len(newGraph)] += 1
-                        else:
-                            # print("already exists!")
-                            # print("NOT")
-                            pass
-                        # print()
-                        # input("press enter...")
-        # print("NEW ONES : ", newOnes)
-        assert isLevel(newOnes)
-        # print(newOnes)
-        # print(newOnes)
-        newGroupByLength = groupByLength(newOnes)
-        assert isListOfLevels(newGroupByLength)
-        # assert isListOfLevels(newGroupByLength)
-        graphsByVerticeNumber[thisNumberOfVertices] = newGroupByLength
-        thisNumberOfVertices += 1
-        # print(
-        #     "Length counter for #vertices ", thisNumberOfVertices, " -> ", lengthCounter
-        # )
-        # print("Number of total graphs: ", len(newOnes))
-    # print("BAMBAM ", graphsByVerticeNumber[3])
-    return graphsByVerticeNumber[numberOfVertices]
-
-
-def groupByLength(xs):
-    returner = {}
-    for x in xs:
-        assert isGraph(x)
-        l = len(x)
-        if l in returner.keys():
-            returner[l].append(x)
+    for cur_candidate in all_graphs:
+        cur_candidate_graph: nx.Graph = nx.from_edgelist(cur_candidate)
+        found_match: bool = False
+        for isomorphic_graph in isomorphic_graphs:
+            if nx.is_isomorphic(cur_candidate_graph, isomorphic_graph):
+                found_match = True
+                break
+        if not found_match:
+            isomorphic_graphs.append(cur_candidate_graph)
+            how_many_useless_candidates_after_all_are_found = 0
         else:
-            returner[l] = [x]
+            how_many_useless_candidates_after_all_are_found += 1
 
-    # print("KEEEEYS  ", list(returner.keys()))
-    returnerList = []
-    for i in returner:
-        # print("appending ", i, "...")
-        # print(returner[i])
-        assert isLevel(returner[i])
-        returnerList.append(returner[i])
-    # print("RETLIST ", returnerList)
-    return returnerList
+    return isomorphic_graphs, how_many_useless_candidates_after_all_are_found
 
 
-def getNumberOfDegrees(xs):
-    vertices = {}
-    for xy in xs:
-        for i in xy:
-            if i in vertices.keys():
-                vertices[i] += 1
+def get_all_graphs_from_degree_array(
+    degree_array: list[int], number_of_vertices: int
+) -> list:
+    """Showcases calling the solver to search for all solutions."""
+    # Creates the model.
+    model = cp_model.CpModel()
+
+    # Creates the variables.
+    nodesVars = []
+    biggestDegree = max(degree_array)
+    smallestDegree = min(degree_array)
+
+    edgesDict = {}
+    allEdges = []
+    for i in range(1, number_of_vertices + 1):
+        v = model.NewIntVar(smallestDegree, biggestDegree, "VertDeg_" + str(i))
+        nodesVars.append(v)
+        for j in range(i + 1, number_of_vertices + 1):
+            e = model.NewIntVar(0, 1, "Edge_" + str(i) + "_" + str(j))
+            if i in edgesDict.keys():
+                edgesDict[i].append(e)
             else:
-                vertices[i] = 1
-    return vertices
+                edgesDict[i] = [e]
+            if j in edgesDict.keys():
+                edgesDict[j].append(e)
+            else:
+                edgesDict[j] = [e]
+            allEdges.append(e)
+
+    for i in range(len(degree_array)):
+        model.Add(nodesVars[i] == degree_array[i])
+
+        model.Add(sum(edgesDict[i + 1]) == nodesVars[i])
+
+    solver = cp_model.CpSolver()
+    solution_printer = VarArraySolutionPrinter(allEdges)
+    status = solver.SearchForAllSolutions(model, solution_printer)
+
+    return solution_printer.solutionArray()
 
 
-def getAmountOfDegrees(xs):
-    vertsDeg = list(getNumberOfDegrees(xs).values())
-    degrees = {}
-    for i in range(1, len(vertsDeg)):
-        degrees[i] = 0
-    for i in vertsDeg:
-        degrees[i] += 1
-    return list(degrees.values())
+def my_print(arg) -> None:
+    if WITH_PRINT:
+        print(arg)
 
 
-def allGraphsStatistics(xs, degrees=False):
-    sumOfAll = 0
-    for li in xs:
-        print("LENGHT ", len(li[0]), " == ", len(li))
-        sumOfAll += len(li)
-    print("\nSum of ALL: ", sumOfAll)
-
-    degQuantKeeper = []
-    repeatedCounter = 0
-    print("\n\n\n")
-    if degrees:
-        for li in xs:
-            for xs in li:
-                gn = getNumberOfDegrees(xs)
-                gnSum = sum(gn.values()) / len(xs)
-                degArr = getAmountOfDegrees(xs)
-                print(
-                    "LENGTH: ",
-                    len(xs),
-                    " || Degrees: ",
-                    list(gn.values()),
-                    " || Degrees Quant. ",
-                    degArr,
-                    " |||| EDGES: ",
-                    xs,
-                    # " // ratio of sum(degrees)/nEdges: ",
-                    # gnSum,
-                )
-                if degArr in degQuantKeeper:
-                    # print("DEGREE ARRAY REPEATED!")
-                    repeatedCounter += 1
-                else:
-                    degQuantKeeper.append(degArr)
-        print("REPEATED DEGREES SETUPS: ", repeatedCounter)
-
-
-def isLevel(xs):
-    for xx in xs:
-        if not isGraph(xx):
+def is_valid_degree_sequence(degree_sequence) -> bool:
+    degree_sequence = sorted(degree_sequence, reverse=True)
+    while degree_sequence:
+        if degree_sequence[0] < 0 or degree_sequence[0] >= len(degree_sequence):
             return False
-    return True
-
-
-def isGraph(xs):
-    for xx in xs:
-        if type(xx[0]) != int or type(xx[0]) != int:
-            return False
-    return True
-
-
-def isListOfLevels(xs):
-    for xx in xs:
-        if not isLevel(xx):
+        current_degree = degree_sequence[0]
+        degree_sequence = degree_sequence[1:]
+        for i in range(current_degree):
+            degree_sequence[i] -= 1
+        degree_sequence = sorted(degree_sequence, reverse=True)
+        if any(x < 0 for x in degree_sequence):
             return False
     return True
 
 
 if __name__ == "__main__":
-    g1 = [[1, 2], [3, 1]]
-    g2 = [[1, 2], [3, 2]]
-    assert isGraph(g1)
-    print(compareGraph(g1, g2, 3))
 
-    # print(allCombs(5))
-    g = [[[1, 2], [3, 1]], [[1, 2], [3, 1], [3, 2]]]
-    g = [
-        [[1, 2], [3, 1], [4, 1]],
-        [[1, 2], [3, 1], [4, 2]],
-        [[1, 2], [3, 1], [4, 1], [4, 2]],
-        [[1, 2], [3, 1], [4, 2], [4, 3]],
-        [[1, 2], [3, 1], [4, 1], [4, 2], [4, 3]],
-        [[1, 2], [3, 1], [3, 2], [4, 1], [4, 2], [4, 3]],
-    ]
-    assert isLevel(g)
-    gg = groupByLength(g)
-    assert isListOfLevels(gg)
-    # print("GG ", gg)
-    # print(len(gg))
-    # allGraphsStatistics(gg, 3)
-    # 6 / 0
-    numberOfVertices = 7
-    zz = getAllGraphsWithNumberOfVertices(numberOfVertices, allConnected=True)
-    saveToJson(zz, numberOfVertices)
-    # print(zz)
-    allGraphsStatistics(zz, degrees=False)
-    # for xx in zz:
-    #    print(xx)
+    print(ASCII_ART)
+
+    n_vertices: int = int(
+        input("Please insert the number of vertices of your Graph").strip()
+    )
+
+    total_isomorphic_graphs: int = 0
+
+    min_number_of_edges, max_number_of_edges = get_interval_of_potential_edges(
+        n_vertices
+    )
+
+    for cur_number_of_edges in tqdm(
+        range(min_number_of_edges, max_number_of_edges + 1)
+    ):
+        my_print(f"Current number of EDGES: {cur_number_of_edges}")
+
+        all_degree_combinations = get_all_degree_combinations(
+            num_vertices=n_vertices,
+            degree_sum=cur_number_of_edges * 2,
+        )
+
+        valid_degree_combinations = [
+            degree_combination
+            for degree_combination in all_degree_combinations
+            if is_valid_degree_sequence(degree_combination)
+        ]
+
+        for degree_combination in tqdm(valid_degree_combinations):
+            my_print(f"Current Degree combination: {degree_combination}")
+
+            all_graphs_found = get_all_graphs_from_degree_array(
+                degree_combination, n_vertices
+            )
+            my_print(f"Number of ALL Graphs found: {len(all_graphs_found)}")
+
+            all_unique_graphs_found, useless_counter = filter_out_isomorphic(
+                all_graphs_found
+            )
+            total_isomorphic_graphs += len(all_unique_graphs_found)
+            assert len(all_unique_graphs_found) > 0
+
+            my_print(
+                f"Number of ALL UNIQUE Graphs found: {len(all_unique_graphs_found)}"
+            )
+            my_print(f"Useless counter: {useless_counter}")
+
+            my_print("-----")
+        my_print("\n")
+
+    print(f"TOTAL GRAPHS OF {n_vertices} VERTICES: {total_isomorphic_graphs}")
